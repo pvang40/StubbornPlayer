c06c000e48b55707dc6536c8b28332a2a3a07bdf
pvang40 <pvang40@mail.csuchico.edu>
2018-02-28 17:37:13 -0800

    added loop

--- main.cpp
+++ main.cpp
@@ -6,6 +6,24 @@ using namespace std;
 int main()
 {
   SecretDoor game;
-    
+  int timesPlaying; 
+  int timesWon = 0; 
+
+  cout<< "How many times do you want to play the game?\n"; 
+  cin>> timesPlaying; 
+
+  for (int i = 0 ; i < timesPlaying ; i++) 
+  { 
+   game.newGame() ;
+   game.guessDoorC() ;
+   game.guessDoorC() ; 
+
+   if (game.isWinner() == true ) 
+   { 
+    timesWon++; 
+   } 
+  } 
+
+ cout<< "You won " << timesWon << " times.\n"; 
   return 0;
 }
